8332c969a2dc84e719dc81a46dcfae3b44523961
ferencek <>
2012-05-04 00:36:53 +0000

    initial commit

---
+++ scripts/run_limits_condor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CONDOR_PROCESS=$1
+BR=$2
+FINAL_STATE=$3
+RUN_DIR=$4
+
+
+START_TIME=`/bin/date`
+echo "started at $START_TIME"
+echo ""
+
+source /uscmst1/prod/sw/cms/shrc prod
+cd $RUN_DIR
+eval `scramv1 runtime -sh`
+cd -
+
+
+let "MASS = $CONDOR_PROCESS*100 + 1000"
+LOG=`echo "stats_${MASS}_${BR}.log"`
+
+echo "Running: stats $MASS $BR $FINAL_STATE > $LOG 2>&1"
+/uscms/home/ferencek/MyAnalysis/MyAnalyzer/test/LimitCode/stats $MASS $BR $FINAL_STATE > $LOG 2>&1
+exitcode=$?
+
+echo ""
+END_TIME=`/bin/date`
+echo "finished at $END_TIME"
+exit $exitcode
